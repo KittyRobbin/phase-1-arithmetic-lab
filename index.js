72a22a85876542222212656035ccc78d8c80fd3d
Elizabeth <elizabeth.muthoni@student.moringaschool.com>
2023-09-29 11:46:27 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+const random = Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 1)) + 1;
+
+ const num3 = 20;
+ const num4 = 8; 
+
+ const mod = num3 % num4;
+
+ console.log(mod)
+
+ const numbers = [5,13,16,20,18];
+ const max = Math.max(...numbers);
+
+ console.log(max);
